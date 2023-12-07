6cf028ff6c34d6edadd41aeb708f026824c112b1
Sheila Gea <sheilagea@gmail.com>
2023-12-07 14:47:52 +0100

    update_scene

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    [SerializeField] private float speed = 2f;
+    [SerializeField] private float speed = 1f;
     private void Update()
     {
         transform.Rotate(0, 0, 360 * speed * Time.deltaTime);
