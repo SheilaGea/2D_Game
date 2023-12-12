d499090570d1b9d552c742c1a3336bd2d98b9a86
Sheila Gea <sheilagea@gmail.com>
2023-12-12 17:29:43 +0100

    add_flag_anim_and_new_assets

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,28 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class finish : MonoBehaviour
 {
     private AudioSource finishSound;
-    
+    private bool levelCompleted = false;
+    private Animator anim;
+
     private void Start()
     {
+        anim = GetComponent<Animator>();
         finishSound = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" && !levelCompleted)
         {
             finishSound.Play();
-            CompleteLevel();
+            anim.SetTrigger("finish");
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
         }
     }
 
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 }
