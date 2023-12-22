4913ed0eb2f08cbdd481326cf948b8ce036824db
Sheila Gea <sheilagea@gmail.com>
2023-12-22 18:58:00 +0100

    health_manager_ui

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -9,17 +9,20 @@ public class HealthManager : MonoBehaviour
     public int health = 3;
 
     public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (Image img in hearts)
+        {
+            img.sprite = emptyHeart;
+        }
+        for (int i = 0; i < health; i++)
+        {
+            hearts[i].sprite = fullHeart;
+        }
     }
 }
